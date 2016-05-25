05726cb7b79a960e82df3e4e1f85c5b60fc0eceb
Jack Lightbody <jack.lightbody@gmail.com>
2016-05-25 11:23:23 -0500

    Small bug fixes

--- character_recognizer.py
+++ character_recognizer.py
@@ -4,19 +4,23 @@
 
 import numpy 
 from Node import *
-from Character import *
 
 def load_chars(filename):
     f = open(filename, 'r')
     chars = []
-
+    matrix = [] 
+    inp = []
     for line in f:
         line = line.rstrip()
         data_list = line.split('\t')
-        newChar = Character(data_list)
-        chars.append(newChar)
-    
-    return chars
+        pixels = list(map(int, data_list[6:]))
+        inp.append(pixels)
+        charVal = data_list[1]
+        desired = ord(charVal) - 97
+        arr = numpy.zeros(26)
+        arr[desired] = 1
+        matrix.append(arr)
+    return inp, matrix
 
 ## Creates input and output list tuple from Character list
 def create_training_data(chars):
@@ -49,7 +53,7 @@ def logistic(x):
     return 1 / (1 + numpy.exp(-x))
 
 def dlogistic(x):
-    return x * (1 - x)
+    return (1 - x)
 
 def sum_squared_error(target, actual):
     return (target - actual) ** 2
@@ -71,22 +75,20 @@ def delta(eta, weight, target, inp, error_fn, derivative_fn):
     return eta * error_fn(target, weight * inp) * derivative_fn(weight * inp) * inp
 
 def learn(inputs, targets, iterations, hidden, eta):
-    weights0 = numpy.random.randn(hidden, len(inputs))
-    weights1 = numpy.random.randn(len(numpy.unique(targets)), hidden)
+    weights0 = numpy.random.randn(hidden, len(inputs[0]))
+    weights1 = numpy.random.randn(26, hidden)
     input_nodes = []
     hidden_nodes = []
     output_nodes = []
-
     for inp in inputs[0]:
         input_nodes.append(Node(0))
     for hid in range(hidden):
         hidden_nodes.append(Node(0, input_nodes, weights0[hid]))
-    for target in range(len(numpy.unique(targets))):
+    for target in range(26):
         output_nodes.append(Node(0, hidden_nodes, weights1[target]))
     for i in range(iterations):
-        print len(inputs)
+        print i
         for j in range(len(inputs)):
-            print j
             ## propagate inputs all the way to the output layer
             for k in range(len(inputs[j])):
                 input_nodes[k].activation = inputs[j][k]
@@ -99,42 +101,37 @@ def learn(inputs, targets, iterations, hidden, eta):
                 for l in range(len(output_nodes[k].inputs)):
                     weight = output_nodes[k].weights[l]
                     inp = output_nodes[k].inputs[l].activation
-                    weight += delta(eta, weight, targets[k], inp, sum_squared_error, dlogistic)
+                    deltav = delta(eta, weight, targets[j][k], inp, sum_squared_error, dlogistic)
+                    weight += deltav
+                output_nodes[k].activation = 0
             for k in range(len(hidden_nodes)):
                 sum_di = 0
                 for l in range(len(output_nodes)):
                     weight = output_nodes[l].weights[k]
-                    sum_di += weight * di(weight, targets[l], hidden_nodes[k].activation, dlogistic)
+                    sum_di += weight * di(weight, targets[j][l], hidden_nodes[k].activation, dlogistic)
                 for l in range(len(hidden_nodes[k].inputs)):
                     weight = hidden_nodes[k].weights[l]
                     inp = hidden_nodes[k].inputs[l]
                     dj = sum_di * dlogistic(inp.activation)
                     weight += eta * dj * inp.activation
-            ## reset activation values
-            for hidden in hidden_nodes:
-                hidden.activation = 0
-            for output in output_nodes:
-                output.activation = 0
-            
+                hidden_nodes[k].activation = 0  
     return input_nodes, hidden_nodes, output_nodes
 
 def predictOutputs(inputs, input_nodes, hidden_nodes, output_nodes, output_fn):
     results = []
     for inputVal in inputs:
-        output_nodes = predict(inputVal, input_nodes, hidden_nodes, output_nodes[:])
-        results.append(output_fn(output_nodes))
-        for hidden in hidden_nodes:
-            hidden.activation = 0
-        for output in output_nodes:
-            output.activation = 0
+        out = predict(inputVal, input_nodes, hidden_nodes, output_nodes)
+        results.append(output_fn(out))
     return results
 
 def predict(inputVal, input_nodes, hidden_nodes, output_nodes):
     for i in range(len(input_nodes)):
         input_nodes[i].activation = inputVal[i]
     for hidden in hidden_nodes:
+        hidden.activation = 0
         hidden.activate()
     for output in output_nodes:
+        output.activation = 0
         output.activate()
     return output_nodes
         
